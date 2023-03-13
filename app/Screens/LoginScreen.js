8e1cc48973e9c911f39bb0ed05e9aca46fac5491
Amit Kumar Goswami <cseiubatgoswami@gmail.com>
2023-03-14 03:59:45 +0600

    second commit

--- app/Screens/LoginScreen.js
+++ app/Screens/LoginScreen.js
@@ -1,12 +1,47 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import React, { useState } from 'react'
 import Screen from '../components/Screen'
 import colors from '../config/colors'
+import Form from '../components/forms/Form'
+import ErrorMessage from '../components/forms/ErrorMessage'
+import FormField from '../components/forms/FormField'
+import SubmitButton from '../components/forms/SubmitButton'
+
 
 const LoginScreen = () => {
   return (
     <Screen style={styles.container}>
-        <Text>LoginScreen</Text>
+        <Image style={styles.logo} source={require("../assets/logo-hospital.png")} />
+        
+        <Form
+            initialValues={{
+              email:'',
+              password:''
+            }}
+        
+            // onSubmit={handleSubmit}
+            // validationSchema={validationSchema}
+        >
+              <ErrorMessage error="Invalid Email/password" />
+                <FormField
+                  maxLength={255}
+                  name="email"
+                  placeholder="Your Email"
+                />
+                <FormField
+                  
+                  name="password"
+                  placeholder="Your Password"
+                  secureTextEntry={true}
+                />
+                <SubmitButton title="Login" />
+                
+                <TouchableOpacity style={styles.redirect} onPress={() => navigation.navigate('Register')} >
+                  <Text>Not an Account?</Text>
+                </TouchableOpacity>
+
+        </Form>
+        
     </Screen>
   )
 }
@@ -19,5 +54,11 @@ const styles = StyleSheet.create({
         backgroundColor: colors.grey,
         alignItems: 'center',
         justifyContent: 'center',
-    }
+    },
+
+    logo:{
+      height: 150,
+      width: 150,
+      alignSelf:'center',
+    },
 })
---
+++ app/components/Forms.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Forms = () => {
+  return (
+    <View>
+      <Text>Forms</Text>
+    </View>
+  )
+}
+
+export default Forms
+
+const styles = StyleSheet.create({})
